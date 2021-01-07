a5eb29a5a9242d27efc0c9d540d4fd48bb94d56c
i509VCB <git@i509.me>
2021-01-07 16:06:35 -0600

    Setup entrypoints

--- entrypoint.zsh
+++ entrypoint.zsh
@@ -1,1 +1,20 @@
- 
+# Get value for machine variable
+source ~/dotfiles/common/machine.zsh
+
+if ($MACHINE -eq 0) {
+    echo "You need to set MACHINE variable in \"entrypoint.zsh\""
+}
+
+source ~/dotfiles/common/common.zsh
+
+if ($MACHINE = "bikeshed") {
+    source ~/dotfiles/bikeshed/bikeshed.zsh
+    exit
+}
+
+if ($MACHINE = "escalator") {
+    source ~/dotfiles/escalator/escalator.zsh
+    exit
+}
+
+echo "Invalid MACHINE value $MACHINE"
