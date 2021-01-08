3c0ed6addc5a60229f7a598992082d2b388ad2a7
i509VCB <git@i509.me>
2021-01-08 00:29:04 -0600

    Copy over the themes

--- common/common.zsh
+++ common/common.zsh
@@ -1,3 +1,4 @@
 # A common file between dotfile configurations for zsh
 source ~/dotfiles/common/aliases.zsh
 source ~/dotfiles/common/ls_colors.zsh
+source ~/dotfiles/common/theming.zsh
--- common/theming.zsh
+++ common/theming.zsh
@@ -1,1 +1,5 @@
- 
+# p10k
+source ~/powerlevel10k/powerlevel10k.zsh-theme 
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/dotfiles/common/p10k.zsh ]] || source ~/dotfiles/common/p10k.zsh
