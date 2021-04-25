9e00b5bcb9e800728f30945b82cceba879083e11
i509VCB <git@i509.me>
2021-04-24 21:01:36 -0500

    Fail if accent color is not set

--- theming/theming.zsh
+++ theming/theming.zsh
@@ -1,6 +1,10 @@
 # ls colors
 source ~/dotfiles/theming/_ls_colors.zsh
 
-# We expect p10k to be instaleld here.
-source ~/powerlevel10k/powerlevel10k.zsh-theme 
-[[ ! -f ~/dotfiles/theming/_p10k.zsh ]] || source ~/dotfiles/theming/_p10k.zsh
+if (( ${+I5_ACCENT_COLOR} )); then
+	# We expect p10k to be installed here.
+	source ~/powerlevel10k/powerlevel10k.zsh-theme 
+	[[ ! -f ~/dotfiles/theming/_p10k.zsh ]] || source ~/dotfiles/theming/_p10k.zsh
+else
+	echo "I5_ACCENT_COLOR was not set. Not loading p10k"
+fi
