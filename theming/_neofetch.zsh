adabb500fa2e0d9f384b0cb91e6512606138e5f4
i509VCB <git@i509.me>
2021-04-30 17:59:33 -0500

    Slight neofetch tweaks

--- theming/_neofetch.zsh
+++ theming/_neofetch.zsh
@@ -1,4 +1,5 @@
 # Theming for neofetch
+# Sure I could use a config, but I can't seem to figure out how to deal with unwrapping the accent color variable
 
 if (where neofetch >> /dev/null -eq 0) {
 	# Color options
@@ -13,6 +14,8 @@ if (where neofetch >> /dev/null -eq 0) {
 	# Ascii Colors
 	# -------------------
 	# Entire distro icon - Accent
+	# -------------------
+	# Disable GTK theme display
 	alias neofetch='neofetch \
 		--colors \
 		$I5_ACCENT_COLOR \
@@ -28,6 +31,8 @@ if (where neofetch >> /dev/null -eq 0) {
 		$I5_ACCENT_COLOR \
 		$I5_ACCENT_COLOR \
 		$I5_ACCENT_COLOR \
+		--gtk3 off \
+		--gtk2 off \
+		--refresh_rate on \
 	'
-	#  x x x x x x
 }
--- theming/theming.zsh
+++ theming/theming.zsh
@@ -1,11 +1,13 @@
 # ls colors
 source ~/dotfiles/theming/_ls_colors.zsh
-source ~/dotfiles/theming/_neofetch.zsh
 
 if (( ${+I5_ACCENT_COLOR} )); then
 	# We expect p10k to be installed here.
 	source ~/powerlevel10k/powerlevel10k.zsh-theme 
 	[[ ! -f ~/dotfiles/theming/_p10k.zsh ]] || source ~/dotfiles/theming/_p10k.zsh
+
+	# Neofetch
+	source ~/dotfiles/theming/_neofetch.zsh
 else
-	echo "I5_ACCENT_COLOR was not set. Not loading p10k"
+	echo "I5_ACCENT_COLOR was not set. Not loading p10k or neofetch configs"
 fi
