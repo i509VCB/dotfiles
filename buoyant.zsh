b7dd5b927889bc976b262bcb0770c4275cb1a7df
i509VCB <git@i509.me>
2021-05-18 16:19:20 -0500

    A new machine

---
+++ buoyant.zsh
@@ -0,0 +1,13 @@
+# dotfile script for machine with hostname `buoyant`
+# This machine is my mac laptop.
+
+source ~/dotfiles/_instant_prompt.zsh
+
+# Machine specifics
+typeset -g I5_ACCENT_COLOR=24 # Cyan
+typeset -g I5_WHITE_TEXT=15 # White
+
+# Common configuration - make sure to copy over to bikeshed
+source ~/dotfiles/_aliases.zsh
+source ~/dotfiles/theming/theming.zsh
+source ~/dotfiles/_paths.zsh
